92a89cb42263f055d91e8d0883f8ab0efc1e9d66
Christopher Patty <christopher.jon.patty@gmail.com>
2019-05-19 23:35:05 -0600

    Moved posts out of pages root and rewrote urls to root

--- gatsby-node.js
+++ gatsby-node.js
@@ -77,7 +77,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   fmImagesToRelative(node) // convert image paths for gatsby images
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode }).replace('/blog', '')
     createNodeField({
       name: `slug`,
       node,
